5c3f0705902cc6263e5971dccfdcc97649367647
Marie Schweiz <marie.schweiz@gmail.com>
2016-05-05 22:20:23 +0200

    So los gehts

---
+++ life.js
@@ -0,0 +1,29 @@
+// This is life.js
+
+// player zählen
+
+$('.wrapper-count button').on('click', function count(playernumber) {
+    
+    //wie bekomm ich jetzt die spielernummer da rein
+    
+    var playernumber = $('button').parent();    
+    var numberofchange = $('span.player-life').text();
+    var buttontype = $('button').text();
+    
+    // welcher button das war abfragen + oder -
+    
+    if ( buttontype === plus ) {
+        // span finden und nehmen
+            
+    }
+    else {
+        
+    }
+    
+  
+    
+    // Timeout setzen und log schreiben (wie bekomm ich einen timeout in der warteschleife überschrieben?)
+    
+    
+    
+    });
