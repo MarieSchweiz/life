56f2f67441ccace6ee9616074ccb5d47a8169751
Marie Schweiz <marie.schweiz@gmail.com>
2016-05-05 22:20:47 +0200

    Das rockt

--- life.js
+++ life.js
@@ -6,8 +6,10 @@ $('.wrapper-count button').on('click', function count(playernumber) {
     
     //wie bekomm ich jetzt die spielernummer da rein
     
-    var playernumber = $('button').parent();    
-    var numberofchange = $('span.player-life').text();
+    var playernumber = $('button').parent();
+    
+    // Wie zum henker komm ich jetzt an die information welcher button zu wem gehört...parent irgendwas.    
+    var numberofchange = $('span').text();
     var buttontype = $('button').text();
     
     // welcher button das war abfragen + oder -
